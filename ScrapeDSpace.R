90e07a7a82e973c95593d3bd30c55810580645df
ciakovx <clarke.iak@gmail.com>
2014-08-16 17:31:55 -0500

    All metadata merge functions are removed to the MergingStatsWithMetadata.R file. ### community.data: The exported metadata files were  split into two directories: public and dark (so complete.cases didn't need to be run in RunScrapeDSpace). As such the number.of.items needed NA fields removed. ### titles.urls: "community" is scraped from the page in the titles.url function and added as a field. ### stats.barplot.titles: Add comment in stats.barplot.titles to indicate that when items have the same title, the stats don't look good (they're treated as factors and the geom text overlaps).
    Wrap the title to 50 characters for presentation.
    
    Add community.name variable and pull it into ggtitle.
    
    Expand graph to account for geomtext label.
    ###
    visits.cor:
    change variable name from df to titles.df.
    
    Add community.name variable for ggtitle.

--- ScrapeDSpace.R
+++ ScrapeDSpace.R
@@ -38,10 +38,10 @@ community.data <- function(directory){
   
   # These totals were manually derived. The total is generated on the fly and appears as "Now showing items 1-20 of 114."
   # I could have done some string manipulation but decided simply to retrieve the total number of items manually.
-  number.of.items <- c(3156, 52, 3, 13, 89, NA, 140,
+  number.of.items <- c(3156, 52, 3, 13, 89, 140,
                        150, 104, 395, 90, 5, 54, 10,
-                       NA, 9, NA, NA, 15, 1, 1, 75,
-                       93, 261, 131, NA, 102, 3, 2186,
+                       9, 15, 1, 1, 75,
+                       93, 261, 131, 102, 3, 2186,
                        4, 6025, 72, 114)
   
   # Get the path to the community browse-by-title page
@@ -125,8 +125,8 @@ url.str <- function(handle, no.items){
 ##     file.visits: Total view statistics for the item bitstream (downloads)
 titles.urls <- function(z){
   # Create an emply dataframe of three columns and rename those columns.
-  df <- data.frame(matrix(nrow=0, ncol=3))
-  names(df) <- c("title", "url", "id")
+  df <- data.frame(matrix(nrow=0, ncol=4))
+  names(df) <- c("title", "url", "id", "community")
   
   # The first loop parses the first page of the url.str vector. dc.identifier.uri uses the XPath to retrieve the 
   # href tag for permanent handle. dc.title uses the XPath to retrieve item title. It is then converted to utf-8 because some
@@ -138,10 +138,12 @@ titles.urls <- function(z){
     dc.identifier.uri <- xpathSApply(html, "//*[@id=\'aspect_artifactbrowser_ConfigurableBrowse_div_browse-by-title-results\']/ul/li/div/div/a/@href")  
     dc.title <- xpathSApply(html, "//*[@id=\'aspect_artifactbrowser_ConfigurableBrowse_div_browse-by-title-results\']/ul/li/div/div/a/span", xmlValue)  
     dc.title <- iconv(dc.title, "latin1", "utf-8")  
+    community <- xpathSApply(html, "//*[@id='ds-trail']/li[2]/a", xmlValue)
     x.handle <- data.frame("x.handle" = as.numeric(str_sub(dc.identifier.uri, start=15)))
     x <- data.frame("title" = dc.title, 
                     "url" = dc.identifier.uri,
-                    "x.handle" = x.handle)
+                    "x.handle" = x.handle,
+                    "community" = community)
     df <- rbind(df, x)
   }
   
@@ -270,100 +272,15 @@ months.stats <- function(z){
 
 
 
-#####################################################################################################################
-## metadata.merge
-##
-## The metadata.merge function merges the metadata as exported from DSpace with the dataframe returned from titles.urls. 
-## This function requires you to download the metadata from the community
-## from DSpace, with admin privileges. Save the metadata CSV file, with the filename as the community.
-## 
-## Arguments: 
-##   titles.df: Data Frame. Returned from the titles.urls function
-##   community: Numeric. The filename of the CSV of metadata for particular subject (e.g. 10106-1147)
-##
-## Returns:
-##   Data frame of total visits, file visits, and full exported metadata merged
-metadata.merge <- function(titles.df, community){
-  # Read in the CSV for the metadata as exported from ResearchCommons.
-  community.df <- read.csv(file=paste0(getwd(), "/data", "/ExportedMetadata", "/10106-", community, ".csv"), na.strings="") 
-  
-  # These two if loops concatenate columns variously named dc.identifier.uri. and dc.identifier.uri.. (note the periods at the end)
-  if(any(names(community.df == "community.df$dc.identifier.uri."))){
-    community.uri <- ifelse(is.na(community.df$dc.identifier.uri),
-                            as.character(community.df$dc.identifier.uri.),as.character(community.df$dc.identifier.uri))  # Merges the two different uri fields based on NA values
-  }
-  if(any(names(community.df) == "dc.identifier.uri..")){
-    community.uri <- ifelse(is.na(community.df$dc.identifier.uri),
-                            as.character(community.df$dc.identifier.uri..),as.character(community.df$dc.identifier.uri))  # Merges the two different uri fields based on NA values    
-  }
-  
-  # Take away the first 29 characters to leave the handle & returns handle as character, bind the handle 
-  # vector to the metadata dataframe, and merge the visits dataframe with the metadata. Create a column called "department"
-  # that includes the community handle and bind that to the final dataframe.
-  y.handle <-  data.frame("y.handle" = as.numeric(str_sub(community.uri, start=29)))  
-  community.df <- data.frame(community.df, y.handle)  
-  final <- merge(titles.df, community.df, by.x = "x.handle", by.y = "y.handle", all = TRUE)  
-  community <- data.frame("community" = rep(as.character(community), nrow(final)))
-  final <- data.frame(final, community)
-  return(final)
-}
-#####################################################################################################################
-
-
-#####################################################################################################################
-## community.metadata.clean
-##
-## The community.metadata.clean function cleans up inconsistent metadata between the the year, author, and 
-## type fields in the dataframe returned from the metadata.merge function, and pulls together a cleaned up dataframe for graphing/
-## 
-## Arguments: 
-##   merged.df: Data Frame. Returned from metadata.merge function
-##
-## Returns:
-##   A more cleaned up and consistent metadata data frame, with inconsistent metadata fields merged, and irrelevant fields excluded.
-community.metadata.clean <- function(merged.df){
-  # # Merges two different "date issued" fields based on NA values, then merges the with the other 
-  # "date issued" field, if it exists, based on NA values, and puts it into a field called 'year.'
-  # Uses only the first four characters (the year) and converts to factor
-  year <- ifelse(is.na(merged.df$dc.date.issued),
-                 as.character(merged.df$dc.date.issued..), as.character(merged.df$dc.date.issued))  
-  if(any(names(merged.df) == "dc.date.issued.en_US.")){
-    year <- ifelse(is.na(year),
-                   as.character(merged.df$dc.date.issued.en_US.), as.character(year))
-  }
-  year <- data.frame("year" = as.factor(str_sub(year, start=0, end=4)))
-  
-  # Merge two different "author" fields based on NA values, 
-  author <- ifelse(is.na(merged.df$dc.contributor.author),
-                   as.character(merged.df$dc.contributor.author..), as.character(merged.df$dc.contributor.author))  
-  author <- ifelse(is.na(author),
-                   as.character(merged.df$dc.contributor.author.en_US.), as.character(author))
-  if(any(names(merged.df) == "dc.type.en_US.")){
-    type <- merged.df$dc.type.en_US.
-  }
-  if(any(names(merged.df) == "dc.type.en.")){
-    type <- merged.df$dc.type.en.
-  }
-  
-  # Gather the fields together & return them
-  community.df.cl <- data.frame( "title" = merged.df$title, 
-                                 author, 
-                                 year, 
-                                 "community" = merged.df$community, 
-                                 "publisher" = merged.df$dc.publisher.en_US., 
-                                 type,
-                                 "file.visits" = merged.df$file.visits, 
-                                 "total.visits" = merged.df$total.visits)
-  return(community.df.cl)
-}
-#####################################################################################################################
 
 
 #####################################################################################################################
 ## stats.barplot.titles
 ##
 ## stats.barplot.titles prints a barplot of file visits for all items in a community from the dataframe returned from the 
-## titles.urls function. Use this if you do not have access to the metadata, and merely have run the titles.url function to scrape the data. 
+## titles.urls function. Use this if you do not have access to the metadata, and merely have run the titles.url function to scrape the data.
+## This function does not handle labels well for communities that have items with exactly the same title.
+## That will need to be fixed in future implementations.
 ##
 ## Arguments:
 ##    titles.df: Data frame. Returned from titles.urls function
@@ -373,9 +290,10 @@ community.metadata.clean <- function(merged.df){
 ##     A horizontal barplot of all titles in a community, ordered by file visits, and labeled with quantities
 stats.barplot.titles <- function(titles.df, top){
   # Sort title by number of file visits and eliminate titles for which there are no (NA) visits
+  titles.df$title <- str_wrap(titles.df$title, width = 50)
   titles.df$views.ordered <- reorder(titles.df$title, titles.df$file.visits) 
   titles.df <- titles.df[complete.cases(titles.df$file.visits), ]  # 
-  
+  community.name <- titles.df$community[1]
   # If "top" is passed, reorder the df according to visits and take the top first through the value passed to top
   if(!missing(top)){
     titles.df <- titles.df[with(titles.df, order(-file.visits)), ]  
@@ -386,7 +304,8 @@ stats.barplot.titles <- function(titles.df, top){
     geom_bar(stat="identity") +
     coord_flip() +
     geom_text(aes(views.ordered, file.visits, label=file.visits), hjust = -0.2) + #set numeric labels
-    ggtitle(label=paste("Total Downloads Per Item")) +
+    ggtitle(label=paste("Total Downloads Per Item for", community.name)) +
+    scale_y_continuous(expand = c(0.1, 0)) +
     ylab("Downloads") +
     xlab("Item") +
     theme_bw()
@@ -395,62 +314,6 @@ stats.barplot.titles <- function(titles.df, top){
 #####################################################################################################################
 
 
-#####################################################################################################################
-## stats.barplot.merged
-##
-## The stats.barplot.merged function is only to be used with the dataframe returned from the community.metadata.clean function.
-## It prints a barplot of file visits (downloads) from items in a community, and provides the option to color code the titles
-## based on a variable in the dataframe: by author, year, publisher, or type.
-##
-## Arguments:
-##    merged.cl.df: Data frame. Returned from community.metadata.clean function
-##    fill.value: Character vector. Optional. Fill the barplots based on a value from within the merged.cl.df. Options include:
-##      "type"
-##      "author"
-##      "year"
-##      "publisher"
-##    top: Numeric. Optional. A value to limit the number of items graphed. e.g. if you want to see the top ten items, enter 10.
-##
-## Returns:
-##    A horizontal barplot of all titles in a community, ordered by file visits, labeled with quantities, and, optionally
-##    filled according to a particular facet.
-stats.barplot.merged <- function(merged.cl.df, fill.value, top){
-  # Sort title by number of file visits & eliminate titles for which there are no (NA) visits
-  merged.cl.df$views.ordered <- reorder(merged.cl.df$title, merged.cl.df$file.visits) 
-  merged.cl.df <- merged.cl.df[complete.cases(merged.cl.df$file.visits), ]
-  
-  # If "top" is passed, reorder the df according to visits and take the top first through the value passed to top
-  if(!missing(top)){
-    merged.cl.df <- merged.cl.df[with(merged.cl.df, order(-file.visits)), ]  
-    merged.cl.df <- merged.cl.df[1:top, ] 
-  }
-  merged.cl.df$fill.value <- merged.cl.df[ , fill.value]
-  
-  # If fill.value is passed, use that value in the aes call as a fill. Otherwise plot without a fill value.
-  if(!missing(fill.value)){
-    graph <- ggplot(data=merged.cl.df) +
-      aes(views.ordered, file.visits, fill = fill.value) +
-      geom_bar(stat="identity") +
-      coord_flip() +
-      geom_text(aes(views.ordered, file.visits, label=file.visits), hjust = -0.2) + #set text labels
-      #ggtitle(label=paste("File Downloads Per Item:", dpt, "ResearchCommons")) +
-      ylab("File Visits") +
-      xlab("Item") +
-      theme_bw() # Get rid of grayscale background
-  } else{
-    graph <- ggplot(data=merged.cl.df) +
-      aes(views.ordered, file.visits) +
-      geom_bar(stat="identity") +
-      coord_flip() +
-      geom_text(aes(views.ordered, file.visits, label=file.visits), hjust = -0.2) + #set text labels
-      #ggtitle(label=paste("File Downloads Per Item:", publ, "ResearchCommons")) +
-      ylab("File Visits") +
-      xlab("Item") +
-      theme_bw() # Get rid of grayscale background
-  }
-  print(graph)
-}
-#####################################################################################################################
 
 #####################################################################################################################
 ## df.summary stats
@@ -492,13 +355,15 @@ df.summary.stats <- function(titles.df){
 ## Returns:
 ##    Plot of the correlation between file visits and total visits, with the correlation coefficient printed in the upper left
 visit.cor <- function(titles.df){
-  df <- df[complete.cases(df$file.visits), ]
-  r <- cor(df$total.visits, df$file.visits) # Get correlation of variables
-  graph <- ggplot(data=df) +
+  titles.df <- titles.df[complete.cases(titles.df$file.visits), ]
+  r <- cor(titles.df$total.visits, titles.df$file.visits) # Get correlation of variables
+  community.name <- titles.df$community[1]
+  graph <- ggplot(data=titles.df) +
     aes(total.visits, file.visits) +
     geom_point() +
     xlab("Total Visits to Site Page") +
     ylab("File Downloads") +
+    ggtitle(paste("Correlation between page visits and file downloads for", community.name)) +
     theme_bw() + # Use bw theme
     geom_smooth(aes(total.visits, file.visits), method=lm, se=FALSE) +  # Add linear regression line
     geom_text(label=paste("r =", r), x=-Inf, y=Inf, hjust=-0.2, vjust=1.2) # Add label for r in upper lft corner
@@ -516,7 +381,8 @@ visit.cor <- function(titles.df){
 
 
 
-# For future reference, some scraping strategies. I decided not to scrape this data but rather to join my df later with the metadata. Keeping it here for future reference if the
+# For future reference, some scraping strategies. I decided not to scrape this data but rather 
+# to join my df later with the metadata. Keeping it here for future reference if the
 # metadata is not available.
 #
 # This is the less effective way of pulling author/publisher/date info
